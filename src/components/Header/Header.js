2ded44a76452591e8eddd8867e0b5cc43ad22091
Ariful Islam Shuvo <arifulislams063@gmail.com>
2022-04-04 01:37:11 +0600

    cerating navbar use react router

---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
+
+const Header = () => {
+    return (
+        <div className='sticky top-0 bg-white px-12 py-8 flex justify-center md:justify-between'>
+            <div className='hidden md:block'>
+                <Link to="/home" className='font-sans text-2xl font-bold '><span className='text-rose-600'>ROYAL</span> <span className='text-sky-600'>CAR-BD</span></Link>
+            </div>
+            
+            <div className='flex gap-4 text-xl'>
+                <CustomLink to="/home">HOME</CustomLink>
+                <CustomLink to="/reviews">REVIEWS</CustomLink>
+                <CustomLink to="/dashboard">DASHBOARD</CustomLink>
+                <CustomLink to="/blogs">BLOGS</CustomLink>
+                <CustomLink to="/about">ABOUT</CustomLink>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
