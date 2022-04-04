0cd3db1dea2feafe6d76465c2e79412df13cc84f
Ariful Islam Shuvo <arifulislams063@gmail.com>
2022-04-05 01:38:49 +0600

    Ready to deploy

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h2>About page</h2>
+            <h2 className='text-5xl text-center font-bold py-20 text-rose-700'>This is About page </h2>
         </div>
     );
 };
